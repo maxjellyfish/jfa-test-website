e21fb0c6457c3adcd84223f3f1f6661807fa65be
myodgee <maxyodgee@gmail.com>
2022-07-14 09:18:17 +1000

    dataLayer.js

---
+++ js/dataLayer.js
@@ -0,0 +1,19 @@
+(function ($) {
+    "use strict";
+    /*
+    window.gtmId = 'GTM-XXXXXX';
+        (function(){
+            if(window.params && typeof window.params.gtmId != 'undefined' && window.params.gtmId && window.params.gtmId !='null') {
+                window.gtmId = window.params.gtmId;
+                localStorage.setItem("gtmId", window.gtmId);
+            } else if(localStorage && localStorage.getItem("gtmId")){
+                window.gtmId = localStorage.getItem("gtmId");
+            }
+        })();
+        */
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+    });
+    
+})(jQuery);
+
